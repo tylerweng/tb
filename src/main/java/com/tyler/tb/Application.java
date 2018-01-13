bd7df20e09cee9e195b131a6e2ad66ce80e43614
Tyler Weng <tylercweng@gmail.com>
2018-01-13 15:27:11 -0800

    Initialize maven logger test

---
+++ src/main/java/com/tyler/tb/Application.java
@@ -0,0 +1,13 @@
+package com.tyler.tb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("hello world");
+
+    }
+}
